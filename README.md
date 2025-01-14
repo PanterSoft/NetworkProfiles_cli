67ad4b18fcbccac5a21e6f0e5cd5362b195fe575
Nico Mattes <mattesnico@gmail.com>
2025-01-14 10:35:21 +0100

    removed gui this is a cli application

--- README.md
+++ README.md
@@ -21,13 +21,3 @@ swift run NetworkProfiles_cli <config-file-path> [create|delete|help]
 - `help`: Zeigt diese Hilfeinformationen an
 
 Wenn keine Aktion (`create`, `delete`, `help`) angegeben wird, können Sie ein vorhandenes Profil aus der Konfigurationsdatei auswählen und aktivieren.
-
-### Graphical User Interface (GUI)
-
-Um die GUI-Version der Anwendung auszuführen, verwenden Sie den folgenden Befehl:
-
-```sh
-swift run NetworkProfiles_cli gui
-```
-
-Hinweis: Die GUI-Version ist nur auf macOS 11.0 oder neuer verfügbar.
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,14 +1,8 @@
 import Foundation
 
 if #available(macOS 10.15, *) {
-    if CommandLine.arguments.contains("menu") {
-        let configPath = CommandLine.arguments[1]
-        print("Launching menu bar application with config path: \(configPath)")
-        createMenuBarApp(configPath: configPath)
-    } else {
-        print("Launching CLI application")
-        NetworkProfilesCLI.main()
-    }
+    print("Launching CLI application")
+    NetworkProfilesCLI.main()
 } else {
     print("This program requires macOS 10.15 or newer.")
     exit(1)
